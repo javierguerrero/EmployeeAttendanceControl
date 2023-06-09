17cf67ad7570945023f4b7afc36d3ce2e6611596
Javier Guerrero <javier@bertonisolutions.com>
2023-06-08 22:37:36 -0500

    Implementación del microservicio de histórico de asistencias

---
+++ ms.attendances/ms.attendances.api/Program.cs
@@ -0,0 +1,101 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ms.attendances.application.Commands;
+using ms.attendances.application.Mappers;
+using ms.attendances.domain.Repositories;
+using ms.attendances.infrastructure.Data;
+using ms.attendances.infrastructure.Mappers;
+using ms.attendances.infrastructure.Repositories;
+using System.Reflection;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+//Habilitar el ingreso del Bearer toek a través de la interfaz de Swagger
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Users Authentication Api",
+        Version = "v1"
+    });
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Cabecera de autorización JWT. \r\n Introduzca ['Bearer'] [espacio] [Token].",
+    });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                          new OpenApiSecurityScheme {
+                              Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme
+                                                                , Id = "Bearer" }
+                          },new string[] {}
+                    }
+                });
+});
+
+builder.Services.AddScoped(typeof(IAttendanceContext), typeof(AttendanceMongoContext));
+builder.Services.AddScoped(typeof(IAttendanceRepository), typeof(AttendanceRepository));
+
+var automapperConfig = new MapperConfiguration(mapperConfig =>
+{
+    mapperConfig.AddMaps(typeof(AttendanceProfile).Assembly);
+    mapperConfig.AddMaps(typeof(AttendanceMongoProfile).Assembly);
+});
+IMapper mapper = automapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.Services.AddMediatR(typeof(CreateAttendanceCommand).GetTypeInfo().Assembly);
+
+//configurar la autorización del token JWT
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+var privateKey = configuration.GetValue<string>("Authentication:JWT:Key");
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(privateKey)),
+        ValidateLifetime = true,
+        RequireExpirationTime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ms.attendances/ms.attendances.application/Commands/Handlers/CreateAttendanceCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using ms.attendances.domain.Entities;
+using ms.attendances.domain.Repositories;
+
+namespace ms.attendances.application.Commands.Handlers
+{
+    public class CreateAttendanceCommandHandler : IRequestHandler<CreateAttendanceCommand, bool>
+    {
+        private readonly IAttendanceRepository _attendanceRepository;
+        private readonly IMapper _mapper;
+        public CreateAttendanceCommandHandler(IAttendanceRepository attendanceRepository, IMapper mapper)
+        {
+            _attendanceRepository = attendanceRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> Handle(CreateAttendanceCommand request, CancellationToken cancellationToken)
+        {
+            var attendanceRecord = _mapper.Map<AttendanceRecord>(request.AttendanceRequest);
+
+            attendanceRecord.UserName = request.UserName;
+
+            return await _attendanceRepository.CreateAttendance(attendanceRecord);
+        }
+    }
+}
+
+
